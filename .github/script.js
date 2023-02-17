d1f21d83d4095dfc8d9601c0d5bcea3fd30ce4ac
Mugiira KIm <mugiirakimathi@gmail.com>
2023-02-17 14:55:20 +0300

    modify js file

--- .github/script.js
+++ .github/script.js
@@ -3,40 +3,54 @@ const addBookForm = document.getElementById('add-book-form');
 const bookList = document.getElementById('book-list');
 
 function addBook(title, author) {
-	const newBook = { title, author };
-	collection.push(newBook);
-	localStorage.setItem('collection', JSON.stringify(collection));
+  const newBook = { title, author };
+  collection.push(newBook);
+  localStorage.setItem('collection', JSON.stringify(collection));
 
-	const li = document.createElement('li');
-	li.textContent = `${title} by ${author}`;
-	bookList.appendChild(li);
+  const li = document.createElement('li');
+  li.textContent = `${title} by ${author}`;
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.addEventListener('click', () => {
+    removeBook(collection.indexOf(newBook));
+  });
+
+  li.appendChild(removeButton);
+  bookList.appendChild(li);
 }
 
 function removeBook(bookIndex) {
-
-	collection.splice(bookIndex, 1);
-	localStorage.setItem('collection', JSON.stringify(collection));
-
-	bookList.removeChild(bookList.childNodes[bookIndex]);
+  collection.splice(bookIndex, 1);
+  localStorage.setItem('collection', JSON.stringify(collection));
+  bookList.removeChild(bookList.childNodes[bookIndex]);
 }
 
-for (const book of collection) {
-	const li = document.createElement('li');
-	li.textContent = `${book.title} by ${book.author}`;
-	bookList.appendChild(li);
+for (const [index, book] of collection.entries()) {
+  const li = document.createElement('li');
+  li.textContent = `${book.title} by ${book.author}`;
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.addEventListener('click', () => {
+    removeBook(index);
+  });
+
+  li.appendChild(removeButton);
+  bookList.appendChild(li);
 }
 
 addBookForm.addEventListener('submit', event => {
-	event.preventDefault();
-	const title = document.getElementById('title-input').value;
-	const author = document.getElementById('author-input').value;
-	addBook(title, author);
-	addBookForm.reset();
+  event.preventDefault();
+  const title = document.getElementById('title-input').value;
+  const author = document.getElementById('author-input').value;
+  addBook(title, author);
+  addBookForm.reset();
 });
 
 bookList.addEventListener('click', event => {
-	if (event.target.nodeName === 'LI') {
-		const bookIndex = Array.from(bookList.childNodes).indexOf(event.target);
-		removeBook(bookIndex);
-	}
+  if (event.target.nodeName === 'BUTTON') {
+    const bookIndex = Array.from(event.target.parentNode.parentNode.children).indexOf(event.target.parentNode);
+    removeBook(bookIndex);
+  }
 });
